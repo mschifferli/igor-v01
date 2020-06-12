091e1e78fe0e47ed51bc63a97b16f9c6f2b70262
mschifferli <mark.schifferli@gmail.com>
2020-06-12 16:05:40 -0400

    add metronome

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,22 @@ fn cubic_amplifier(input: f32) -> f32 {
     }
 }
 
+fn tick(metronome: i32) -> f32 {
+  if metronome < 1000 {
+    let mut s = 0.5;
+    if metronome < 10 {
+      s = metronome as f32 / 10.0;
+    } else if metronome > 900 {
+      s = 1.0 - (metronome as f32 - 900.0) / 100.0;
+    }
+    (metronome as f32 * 0.05).sin() * s
+  } else {
+    0.0
+  }
+
+}
+
+
 fn run() -> Result<(), pa::Error> {
     let pa = pa::PortAudio::new()?;
 
@@ -86,8 +102,6 @@ fn run() -> Result<(), pa::Error> {
     // Construct the settings with which we'll open our duplex stream.
     let settings = pa::DuplexStreamSettings::new(input_params, output_params, SAMPLE_RATE, FRAMES);
 
-    // Once the countdown reaches 0 we'll close the stream.
-    let mut count_down = 60.0;
 
 
     // Keep track of the last `current_time` so we can calculate the delta time.
@@ -99,10 +113,21 @@ fn run() -> Result<(), pa::Error> {
     // let delay_length: f64 = 0.5;
     // let delay_line: DelayLine = delay_line::DelayLine::new((delay_length * SAMPLE_RATE) as usize );
 
-    let length : usize = (((SAMPLE_RATE  * count_down ) as i32) * CHANNELS) as usize;
+    let delay = 8.0;
+    let tempo = 120.0; // beats per minute
+    let beat = 60.0 / tempo; // how many seconds does a beat last?
+    let samplesPerBeat = (((SAMPLE_RATE * beat ) as i32) * CHANNELS) as i32;
+    let beatsPerRepeat = 4.0;
+    let mut count_down = (beatsPerRepeat + 1.0) * beat;
+    let mut metronome = 0;
+    let barsToRecord = 16.0;
+    let mut duration = barsToRecord * beatsPerRepeat * beat;
+    let length : usize = (((SAMPLE_RATE  * duration ) as i32) * CHANNELS) as usize;
     let mut buffer: Vec<f32> = vec![0.0; length];
     let mut index: usize = 0;
-    let delay = 8.0;
+
+
+
     let JUMP = (SAMPLE_RATE * delay ) as i32;
     // A callback to pass to the non-blocking stream.
     let callback = move |pa::DuplexStreamCallbackArgs {
@@ -115,52 +140,61 @@ fn run() -> Result<(), pa::Error> {
         let current_time = time.current;
         let prev_time = maybe_last_time.unwrap_or(current_time);
         let dt = current_time - prev_time;
-        count_down -= dt;
         maybe_last_time = Some(current_time);
 
         assert!(frames == FRAMES as usize);
-        
-
-        // Pass the input through the fuzz filter and then to the output
-        // BEWARE OF FEEDBACK!
-
         let mut o : f32 = 0.0;
-        let mut i2 : i32;
-        let ATT: f32 = 0.5;
-        let mut attenuation: f32 = ATT;
 
-
-
-        for (output_sample, input_sample) in out_buffer.iter_mut().zip(in_buffer.iter()) {
-            o = fuzz(*input_sample);
-            i2 = (index as i32) - JUMP;
-            attenuation = 0.9;
-            buffer[index] = o;
-            index = index + 1;
-            if index >= length {
-              index = 0;
-            }
-
-            while i2 >= 0 {
-              o += buffer[i2 as usize] * attenuation;
-              i2 -= JUMP;
-              attenuation *= ATT;
-            }
-            // if i2 >= 0 {
-            //     *output_sample = fuzz(*input_sample + buffer[i2 as usize])
-            // } else {
-            //     *output_sample = fuzz(*input_sample)
-            // }
-            *output_sample = o
-        }
-
-        sender.send(o).ok();
         if count_down > 0.0 {
-            pa::Continue
+          count_down -= dt;
+          for (output_sample, input_sample) in out_buffer.iter_mut().zip(in_buffer.iter()) {
+              let mut o = tick(metronome);
+              metronome += 1;
+              if metronome == samplesPerBeat {
+                metronome = 0;
+              }
+              *output_sample = o
+          }          
+          sender.send(count_down).ok();
+          pa::Continue
         } else {
-            println!("{:?}", buffer);
-            pa::Complete
-        }  
+          // Pass the input through the fuzz filter and then to the output
+          // BEWARE OF FEEDBACK!
+          duration -= dt;
+          
+          let mut i2 : i32;
+          let ATT: f32 = 0.5;
+          let mut attenuation: f32 = ATT;
+          for (output_sample, input_sample) in out_buffer.iter_mut().zip(in_buffer.iter()) {
+              let mut o = tick(metronome);
+              metronome += 1;
+              if metronome == samplesPerBeat {
+                metronome = 0;
+              }
+              o += fuzz(*input_sample);
+              i2 = (index as i32) - JUMP;
+              attenuation = 0.9;
+              buffer[index] = o;
+              index = index + 1;
+              if index >= length {
+                index = 0;
+              }
+              while i2 >= 0 && attenuation > 0.05 {
+                o += buffer[i2 as usize] * attenuation;
+                i2 -= JUMP;
+                attenuation *= ATT;
+              }
+              *output_sample = o
+          }
+
+          sender.send(o.into()).ok();
+          if duration > 0.0 {
+              pa::Continue
+          } else {
+              println!("{:?}", buffer);
+              pa::Complete
+          } 
+        }
     };
 
     // Construct a stream with input and output sample types of f32.
